55fb4b789dc51af20635bb6dc42dec276343796b
vaishnavimandage <mandagevaishnavi@cityuniversity.edu>
2023-09-07 13:06:48 -0700

    Added Frontend

---
+++ frontend/src/components/login.js
@@ -0,0 +1,13 @@
+//HOS10A login.js
+import React from "react";
+
+const Login = () => {
+  return (
+    <a href="http://localhost:5050/facebook" 
+    className="btn btn-primary">
+      Login with Facebook
+    </a>
+  );
+};
+
+export default Login;
